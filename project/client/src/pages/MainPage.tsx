305c2c9afe218c4125579929a4499a321d168ca8
codekyz <codekyz0111@gmail.com>
2023-10-14 17:50:13 +0900

    design: replace section title with CommonTitle

--- project/client/src/pages/MainPage.tsx
+++ project/client/src/pages/MainPage.tsx
@@ -1,8 +1,6 @@
 import { Box } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
-import CommonButton from "components/common/CommonButton";
 import MainSection from "components/main/MainSection";
-import CommonTypography from "components/common/CommonTypography";
 import MainBookProgressCard from "components/main/MainBookProgressCard";
 import MainBookRankCard from "components/main/MainBookRankCard";
 import CommonSearchBar from "components/common/CommonSearchBar";
@@ -13,34 +11,13 @@ const MainPage = () => {
     <Box>
       {/* 도서검색 */}
       <MainSection>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "end",
-          }}
-        >
-          <CommonTypography value="📚 도서 검색" bold={true} variant="h5" />
-        </Box>
+        <CommonTitle value="📚 도서 검색" />
         <CommonSearchBar />
       </MainSection>
 
       {/* 독서진행률 */}
       <MainSection>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "end",
-            mb: 2,
-          }}
-        >
-          <CommonTypography
-            value="📚 완독까지 이만큼 남았어요"
-            bold={true}
-            variant="h5"
-          />
-          <CommonButton value="책 추가하기" />
-        </Box>
+        <CommonTitle value="📚 완독까지 이만큼 남았어요" />
         <MainBookProgressCard isNonMember={true} />
         <MainBookProgressCard isComplete={true} />
         <MainBookProgressCard isNonMember={false} />
@@ -50,19 +27,7 @@ const MainPage = () => {
 
       {/* 인기도서 */}
       <MainSection>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "end",
-            mb: 2,
-          }}
-        >
-          <CommonTypography
-            value="📚 인기도서 TOP10"
-            bold={true}
-            variant="h5"
-          />
-        </Box>
+        <CommonTitle value="📚 인기도서 TOP10" />
 
         {/* 도서container */}
         <Grid container spacing={2} columns={{ xs: 1, md: 10 }}>
