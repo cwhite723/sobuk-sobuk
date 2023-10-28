dcf3f438bfcab22d54475e1b61a101b31cf06253
codekyz <codekyz0111@gmail.com>
2023-10-29 03:52:19 +0900

    feat: add snackbar to  access management

--- project/client/src/pages/auth/NotPrivateRoute.tsx
+++ project/client/src/pages/auth/NotPrivateRoute.tsx
@@ -1,9 +1,30 @@
-import { Navigate } from "react-router-dom";
+import { Alert, Snackbar } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const NotPrivateRoute = ({ children }: { children: React.ReactNode }) => {
   const isLoggedIn = localStorage.getItem("token");
+  const navigate = useNavigate();
 
-  return isLoggedIn ? <Navigate to="../" /> : <>{children}</>;
+  const handleClose = () => {
+    navigate("../");
+  };
+
+  return isLoggedIn ? (
+    <>
+      <Snackbar
+        open={true}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+      >
+        <Alert severity="error" variant="filled" onClose={handleClose}>
+          이미 로그인 되어 있습니다.
+        </Alert>
+      </Snackbar>
+    </>
+  ) : (
+    <>{children}</>
+  );
 };
 
 export default NotPrivateRoute;
--- project/client/src/pages/auth/PrivateRoute.tsx
+++ project/client/src/pages/auth/PrivateRoute.tsx
@@ -1,9 +1,30 @@
-import { Navigate } from "react-router-dom";
+import { Alert, Snackbar } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
   const isLoggedIn = localStorage.getItem("token");
+  const navigate = useNavigate();
 
-  return isLoggedIn ? <>{children}</> : <Navigate to="../login" />;
+  const handleClose = () => {
+    navigate("../login");
+  };
+
+  return isLoggedIn ? (
+    <>{children}</>
+  ) : (
+    <>
+      <Snackbar
+        open={true}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+      >
+        <Alert severity="error" variant="filled" onClose={handleClose}>
+          로그인이 필요합니다.
+        </Alert>
+      </Snackbar>
+    </>
+  );
 };
 
 export default PrivateRoute;
