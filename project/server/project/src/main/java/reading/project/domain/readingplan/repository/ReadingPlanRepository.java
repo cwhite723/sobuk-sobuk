86571b67b189c36db4ac7b3cd674e02a59f7339a
cwhite723 <c_white723@naver.com>
2023-11-01 20:00:17 +0900

    ✨ 독서 기록 조회 기능 개발

--- project/server/project/src/main/java/reading/project/domain/readingplan/repository/ReadingPlanRepository.java
+++ project/server/project/src/main/java/reading/project/domain/readingplan/repository/ReadingPlanRepository.java
@@ -3,5 +3,10 @@ package reading.project.domain.readingplan.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import reading.project.domain.readingplan.entity.ReadingPlan;
 
-public interface ReadingPlanRepository extends JpaRepository<ReadingPlan, Long> {
+import java.util.List;
+
+import static reading.project.domain.readingplan.entity.ReadingPlan.*;
+
+public interface ReadingPlanRepository extends JpaRepository<ReadingPlan, Long>, ReadingPlanRepositoryCustom {
+    List<ReadingPlan> findByStatus(Status status);
 }
---
+++ project/server/project/src/main/java/reading/project/domain/readingplan/service/Scheduler.java
@@ -0,0 +1,23 @@
+package reading.project.domain.readingplan.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@EnableScheduling
+@RequiredArgsConstructor
+public class Scheduler {
+    private final ReadingPlanService readingPlanService;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void readingPlanScheduler() {
+        log.info("스케줄러 작동 시작");
+        readingPlanService.calculatePagesPerDay();
+        readingPlanService.checkStatus();
+        log.info("스케줄러 작동 종료");
+    }
+}
