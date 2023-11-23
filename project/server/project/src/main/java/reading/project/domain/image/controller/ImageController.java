b22d9483d94d906f3458bc85478a442f64220d36
cwhite723 <c_white723@naver.com>
2023-11-23 13:20:53 +0900

    ♻️ contentLength 지정

--- project/server/project/src/main/java/reading/project/domain/image/controller/ImageController.java
+++ project/server/project/src/main/java/reading/project/domain/image/controller/ImageController.java
@@ -1,9 +1,9 @@
 package reading.project.domain.image.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import reading.project.domain.image.dto.ImageResponse;
@@ -17,7 +17,7 @@ public class ImageController {
     private final ImageService imageService;
 
     @PostMapping
-    public ApplicationResponse<ImageResponse> uploadImage(@ModelAttribute MultipartFile file) {
+    public ApplicationResponse<ImageResponse> uploadImage(@RequestPart MultipartFile file) {
         ImageResponse response = imageService.saveImage(file);
 
         return ApplicationResponse.ok(response);
