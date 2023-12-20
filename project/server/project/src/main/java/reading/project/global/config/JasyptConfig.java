6a2a6f4959ddcd92436e7e2aa47b9c87871d8936
cwhite723 <c_white723@naver.com>
2023-12-20 23:45:09 +0900

    ♻️ jasypt 설정 수정

--- project/server/project/src/main/java/reading/project/global/config/JasyptConfig.java
+++ project/server/project/src/main/java/reading/project/global/config/JasyptConfig.java
@@ -1,30 +1,27 @@
 package reading.project.global.config;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.jasypt.encryption.StringEncryptor;
-import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
-import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.NoIvGenerator;
+import org.jasypt.salt.RandomSaltGenerator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableEncryptableProperties
 public class JasyptConfig {
+
     @Value("${jasypt.encryptor.password}")
-    String password;
+    private String password;
 
-    @Bean(name = "jasyptStringEncryptor")
+    @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
-
-        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(password);
-        config.setAlgorithm("PBEWithMD5AndDES");
-        config.setKeyObtentionIterations("1000");
-        config.setPoolSize("1");
-        config.setProviderName("SunJCE");
-        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-        config.setStringOutputType("base64");
-        encryptor.setConfig(config);
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword(password);
+        encryptor.setIvGenerator(new NoIvGenerator());
+        encryptor.setSaltGenerator(new RandomSaltGenerator());
         return encryptor;
     }
 }
